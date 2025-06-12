e983ee7704787f82f0c3899a92c1cbd303fb57ce
Felipe Caetano <126731108+FelipeCaetanoSS@users.noreply.github.com>
2025-06-12 13:50:52 -0300

    exer5.c

--- exer5.c
+++ exer5.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//Ler 5 nomes (strings de até 20 caracteres) e exibir os nomes na ordem inversa.
+// Ler 5 nomes (strings de atÃ© 20 caracteres) e exibir os nomes na ordem inversa.
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    char nomes[5];
-    char nome[20];
+    char nomes[5][20];
 
-    for (i = 1; i < 6; i++){
-    printf("Digite nome desejado:");
-    scanf("%c", &nome);
+    printf("Digite 5 nomes:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Nome %d: ", i + 1);
+        scanf("%19s", nomes[i]);
     }
 
-    printf("Os número(s) negativo(s) são\n");
-    for (i = 1; i < 6; i++){
-
+    printf("\nNomes na ordem inversa:\n");
+    for (int i = 4; i >= 0; i--) {
+        printf("Nome %d: %s\n",i + 1, nomes[i]);
     }
-        }
+
     return 0;
 }

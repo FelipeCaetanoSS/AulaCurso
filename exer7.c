c022f698b5e72e8c9c81a15863fd0a30f91fb024
Felipe Caetano <126731108+FelipeCaetanoSS@users.noreply.github.com>
2025-06-12 14:27:25 -0300

    Update exer7.c

--- exer7.c
+++ exer7.c
@@ -2,24 +2,27 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//Ler 10 números inteiros e contar quantas vezes o número 0 aparece.
+//Ler 10 nÃºmeros inteiros e contar quantas vezes o nÃºmero 0 aparece.
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Portuguese");
+
     int numeros[10];
-    int i;
+    int i, contador = 0;
 
-    for (i = 1; i < 11; i++){
-    printf("Digite o valor %d:", i);
-    scanf("%d", &numeros[i]);
+
+    for (i = 0; i < 10; i++) {
+        printf("Digite o valor %d: ", i + 1);
+        scanf("%d", &numeros[i]);
     }
 
-    printf("Os número(s) negativo(s) são\n");
-    for (i = 1; i < 11; i++){
-    if (numeros[i] < 0){
-    printf("numero %d \n", numeros[i]);
-    }
+    for (i = 0; i < 10; i++) {
+        if (numeros[i] == 0) {
+            contador++;
         }
+    }
+
+    printf("/nO total de nÃºmeros 0 foi: %d \n", contador);
+
     return 0;
 }
